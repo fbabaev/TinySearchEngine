f3ef340c313294351d57e768b6a66b70d840bb9b
Firdavskhon Babaev <161253240+fbabaev@users.noreply.github.com>
2024-08-14 15:13:31 -0700

    Create index.h

---
+++ common/index.h
@@ -0,0 +1,35 @@
+/*
+ * index.c - common directory
+ *
+ * The 'index' module provides the functionality to create, modify, save, and load an index that maps from words to document IDs and counts. This index is used by the 'indexer' and 'querier' modules to facilitate efficient searching of the crawled webpages.
+ *
+ * Firdavskhon Babaev
+ * Date: May 5, 2024
+ */
+
+#ifndef __INDEX_H
+#define __INDEX_H
+
+#include <stdio.h>
+#include "file.h"
+#include "hashtable.h"
+#include "counters.h"
+
+typedef hashtable_t index_t;
+
+/* Create a new index. */
+index_t* index_new(const int num_slots);
+
+/* Add a word with its document ID to the index. */
+void index_add(index_t *index, const char *word, int docID);
+
+/* Delete the index. */
+void index_delete(index_t* index);
+
+/* Save the index to a file. */
+int index_save(FILE* fp, index_t* index);
+
+/* Load the index from a file. */
+index_t* index_load(FILE* fp);
+
+#endif // __INDEX_H
