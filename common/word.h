e277f704f41eafc0a784f907afb02f850180b56f
Firdavskhon Babaev <161253240+fbabaev@users.noreply.github.com>
2024-08-15 17:51:33 -0700

    Update word.h

--- common/word.h
+++ common/word.h
@@ -1,1 +1,17 @@
+/*
+ * word.h - 'word' module
+ *
+ * This header file provides the interface for normalizing a word by converting all its characters to lowercase.
+ *
+ * Firdavskhon Babaev
+ * Date: May 6, 2024
+ */
+
+#ifndef __WORD_H
+#define __WORD_H
+
+/* Normalize a word by converting all characters to lowercase. */
+void normalizeWord(char *word);
+
+#endif // __WORD_H
 
